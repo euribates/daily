899feb85f943bcb0a2e30d73367398dae29ca295
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2022-12-02 13:36:35 +0000

    Mejoras en listado de tareas

--- src/main.py
+++ src/main.py
@@ -8,51 +8,76 @@ import re
 from subprocess import call
 
 from rich.console import Console
+from rich.markdown import Markdown
 import typer
 
 
 SOURCE = Path.home() / Path('Dropbox/parcan')
 EDITOR = os.environ.get('EDITOR', 'vim')  # that easy!
 
-console = Console()
 
-
-def hoy():
+def hoy() -> datetime.date:
     return datetime.date.today()
 
 
-def get_current_note_path():
-    f = hoy()
+def parse_fecha(s) -> datetime.date:
+    if '-' in s:
+        year, month, day = [int(_) for _ in s.split('-')]
+    else:
+        day, month, year = [int(s) for _ in s.split('/')]
+    return datetime.date(year, month, day)
+
+
+def get_current_note_path(f: datetime.date = None) -> Path:
+    f = f or hoy()
     filename = f'journal-{f.year:04d}-{f.month:02d}-{f.day:02d}.md'
     return SOURCE / filename
 
 
-app = typer.Typer()
-
-
-@app.command()
-def today():
+def list_tasks(note: str):
+    console = Console()
     pat_note = re.compile(r"\[(.)\] (.+)")
-    note = get_current_note_path()
-    print(f"Tareas para hoy ({hoy()})")
+    pat_comilla = re.compile(r"`(.+?)`")
     if note.exists():
         with open(note, 'r', encoding='utf-8') as f_in:
             for line in f_in:
                 _match = pat_note.match(line)
                 if _match:
-                    if _match.group(1) in {'x', 'X'}:
-                        markup = 'yellow'
+                    checkbox, titulo = _match.groups()
+                    if checkbox in {'x', 'X'}:
+                        markup = '[yellow]'
                         emoji = ':white_heavy_check_mark:'
                     else:
-                        markup = 'bold green'
+                        markup = '[bold green]'
                         emoji = ':white_large_square:'
-                    titulo = _match.group(2)
+                    titulo = pat_comilla.sub(
+                        lambda m: f"[bold]{m.group(1)}[/bold]",
+                        titulo,
+                    )
                     console.print(
                         emoji,
-                        f"[{markup}]{titulo}[/{markup}]",
-                        sep=' ',
-                        )
+                        markup,
+                        titulo,
+                    )
 
+
+app = typer.Typer()
+
+@app.command()
+def at(fecha:str):
+    fecha = parse_fecha(fecha)
+    note = get_current_note_path(fecha)
+    print(f"Tareas para {fecha}")
+    list_tasks(note)
+
+
+
+
+@app.command()
+def today():
+    note = get_current_note_path()
+    print(f"Tareas para hoy ({hoy()})")
+    list_tasks(note)
 
 
 @app.command()
@@ -70,8 +95,9 @@ def todo(task_name: str):
 
 
 @app.command()
-def edit():
-    note = get_current_note_path()
+def edit(fecha: str = None):
+    f = hoy() if fecha is None else parse_fecha(fecha)
+    note = get_current_note_path(f)
     call([EDITOR, note])
 
 
